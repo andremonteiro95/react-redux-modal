2716725877f546490cc3b8bbcdaf06f5f0cf9bd6
andremonteiro95 <andremonteiro95@gmail.com>
2020-11-25 12:32:31 +0000

    Basic modal display

---
+++ src/components/Modal.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Modal.css';
+
+function Modal() {
+  return (
+    <div className="modal-overlay">
+      <div className="modal">This is my new modal!</div>
+    </div>
+  );
+}
+
+export default Modal;
